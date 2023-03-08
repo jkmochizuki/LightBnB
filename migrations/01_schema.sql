aad39360f4aef9174fcd2320f36f4664c657af0f
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-03-07 20:11:49 -0800

    Create guest_reviews and rates tables

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -3,24 +3,24 @@ DROP TABLE IF EXISTS properties CASCADE;
 DROP TABLE IF EXISTS reservations CASCADE;
 DROP TABLE IF EXISTS property_reviews CASCADE;
 
-CREATE TABLE users(
+CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE properties(
+CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
   description TEXT,
   thumbnail_photo_url VARCHAR(255) NOT NULL,
   cover_photo_url VARCHAR(255) NOT NULL,
-  cost_per_night INTEGER  NOT NULL DEFAULT 0,
-  parking_spaces INTEGER  NOT NULL DEFAULT 0,
-  number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
-  number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
+  cost_per_night INTEGER NOT NULL DEFAULT 0,
+  parking_spaces INTEGER NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE properties(
   active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
-CREATE TABLE reservations(
+CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
@@ -37,7 +37,7 @@ CREATE TABLE reservations(
   end_date DATE NOT NULL
 );
 
-CREATE TABLE property_reviews(
+CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
   -- guest_id and property_id are already included in the reservations table, we would not need to add these columns
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE, 
---
+++ migrations/02_schema.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS rates CASCADE;
+DROP TABLE IF EXISTS guest_reviews CASCADE;
+
+CREATE TABLE rates(
+  id SERIAL PRIMARY KEY NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  cost_per_night INTEGER NOT NULL DEFAULT 0,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL
+);
+
+CREATE TABLE guest_reviews(
+  id SERIAL PRIMARY KEY NOT NULL,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
+);
